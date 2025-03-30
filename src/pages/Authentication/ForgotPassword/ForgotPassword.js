c99f8080d46105a7bd2ba4318ba0641419ae8d9b
JesseSambo19 <joshwhite56565@gmail.com>
2025-03-30 14:03:46 +0200

    added a css class for Forgot Password page

--- src/pages/Authentication/ForgotPassword/ForgotPassword.js
+++ src/pages/Authentication/ForgotPassword/ForgotPassword.js
@@ -143,7 +143,7 @@ const ForgotPassword = () => {
   return (
     <Center>
       <Card className={classes['forgot-password']}>
-        <p>
+        <p className={classes['reset-password-message']}>
           Forgot your password? No problem. Just let us know your email address
           and we will email you a password reset link that will allow you to
           choose a new one.
